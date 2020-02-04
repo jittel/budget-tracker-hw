83a3b6d102116867ac737567d5fb3238d1c4b020
unknown <nathan.c.ginn@gmail.com>
2020-02-03 17:25:15 -0800

    fixed up service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "db.js", "index.js", "style.css", "favicon.ico"];
+const FILES_TO_CACHE = ["/", "index.html", "db.js", "index.js", "style.css", "favicon.ico"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
